97a51ad42a9a26527318cc17ebc0cbb88dcabdd0
Nayan Dey <ndeynayan123@gmail.com>
2026-04-26 03:03:28 +0530

    show dynamic news

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,10 +1,60 @@
+import { getNewsDetailsById } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
+import { CiBookmark, CiShare2 } from "react-icons/ci";
+import { FaEye } from "react-icons/fa";
+import { IoIosStar } from "react-icons/io";
 
-import React from 'react';
 
-const NewsDetailsPage = () => {
+const NewsDetailsPage = async ({ params }) => {
+
+    const { id } = await params;
+    console.log("params", id)
+
+    const news = await getNewsDetailsById(id)
+    console.log("news", news)
+
     return (
-        <div>
-            News Details Page
+        <div className="max-w-3xl mx-auto my-8 card">
+            <div className="card bg-base-100  shadow-sm">
+                <div className="card-body">
+                    <div className='flex justify-between items-center bg-slate-200 p-2'>
+                        <div className='flex gap-1 items-center'>
+                            <Image src={news.author?.img} alt={news.author?.name} height={40} width={40} className='rounded-full'></Image>
+                            <div>
+                                <h2 className='font-semibold'>{news.author?.name}</h2>
+                                <p className='text-xs'>{news.author?.published_date}</p>
+                            </div>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <CiShare2 className='text-xl' />
+                            <CiBookmark className='text-xl' />
+
+                        </div>
+                    </div>
+
+                    <h2 className="card-title">{news.title}</h2>
+
+                    <figure>
+                        <Image src={news.image_url} alt={news.title} width={300} height={300} className='w-full'></Image>
+                    </figure>
+
+                    <p className=''>{news.details}</p>
+
+                    <div className='flex justify-between items-center gap-2'>
+                        <div className='flex items-center gap-5'>
+                            <h2 className='flex items-center gap-2'><IoIosStar className='text-lg text-yellow-500' />
+                                {news.rating.number}</h2>
+
+                            <h2 className='flex items-center gap-2'><FaEye className='text-lg' />{news?.total_view}</h2>
+                        </div>
+
+                        <Link href={`/category/${news.category_id}`}><button className='btn bg-red-500 text-white'>See other news for this category <BsArrowRight></BsArrowRight></button></Link>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/lib/data.js
+++ src/lib/data.js
@@ -9,3 +9,12 @@ export const getNewsByCategoryId = async (category_id) => {
     const data = await res.json();
     return data.data;
 }
+
+
+export const getNewsDetailsById = async (news_id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+    const data = await res.json();
+    return data.data[0];
+}
+
+
